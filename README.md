60026c8eb4fde18981b682fbb146c7ed648d6a26
xArthos <giampaololocascio@gmail.com>
2021-03-02 20:42:43 +0100

    Read.me updated and server modified

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 ## Leviathan
+
+Final Project: Social Game Wikipedia Platform
+Developed by: Giampaolo Nico Lo Cascio - Arthos
+
+Introduction:
+
+    Leviathan aims to create an online Platform where fans of videogames can create theyr own Wikipage about theyr favorite games, share them in the net, comment them and interact with other users registred in the Platform.
+
+Frontend:                       Backeend:
+
+    React                       Node.js
+    React-bootstrap             Express
+    React-redux                 Mongo DB
+    Redux-thunk                 Multer
+    React-router                SendGrid
+    React-moment                Jsonwebtoken
+    React-dropzone              Bcrypt
+    React-fontawesome           Fs                  
+    Superagnet                  Path
+    Axios                       Joi
+                                Mongoose
+                                Body-parser
+                                Cookie-parser
+                                Cors
+                                Dotenv
--- server/server.js
+++ server/server.js
@@ -5,29 +5,17 @@ import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import sgMail from '@sendgrid/mail';
-import session from 'express-session';
-import flash from 'connect-flash';
 import dotenv from 'dotenv';
 const app = express();
 
 // Server Settings
 dotenv.config();                                                        // .env file
 app.use(express.static(`${process.cwd()}/public`));                     // Static Folder
-app.set('view engine', 'hbs');                                          // View Engine
 app.use(cors());                                                        // Cross connection
 app.use(cookieParser());                                                // CookieParser
 app.use(bodyParser.urlencoded({ extended: false }));                    // BodyParser
 app.use(bodyParser.json());                                             // BodyParser Json
 app.use(express.json());                                                // Use Json files
-app.use(flash());                                                       // Flash
-app.use(session({
-    secret: process.env.SESSION_SECRET,
-    cookie: {
-        maxAge: 60000 * 60 * 24 // 1 day
-    },
-    resave: true,
-    saveUninitialized: true
-}));                                                                    // Session
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);                         // SendGrid
 
 // URL Setting
